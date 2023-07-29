dabe20695d990b001b117ad63c0ee034d8ff3c64
Aashish <aashishnkumar@gmail.com>
2023-07-29 15:26:55 +0530

    Successfully added a training data set for alphabets, next will work on words

--- backend/backend.js
+++ backend/backend.js
@@ -1,4 +1,5 @@
 const brain = require("brain.js");
+// const data = require("./trainOnThis.Json");
 
 /*  Traditional way of reading data from a file
     const fs = require("fs");
@@ -48,3 +49,62 @@ const brain = require("brain.js");
 
     main();
 */
+
+const fs = require("fs");
+
+function readTrainingData(filename) {
+  try {
+    const jsonData = fs.readFileSync(filename, "utf-8");
+    return JSON.parse(jsonData);
+  } catch (err) {
+    console.error(err);
+    return [];
+  }
+}
+
+// scale down function
+// function convertToBinary(morseCode) {
+//   return morseCode.split("").map((char) => (char === "." ? 1 : 0));
+// }
+
+function writeTrainingData(filename, data) {
+  try {
+    // // convert the dot and dashes into 0 or 1
+    // const convertedData = data.map((entry) => ({
+    //   input: convertToBinary(entry.input),
+    //   output: entry.output,
+    // }));
+
+    const jsonData = JSON.stringify(data, null, 2);
+    fs.writeFileSync(filename, jsonData, "utf-8");
+    console.log("Success write");
+  } catch (err) {
+    console.error("Error ", err);
+  }
+}
+
+// const oldData = readtraingData("trainOnThis.Json");
+
+// const newData = {
+//   input: "... --- ...",
+//   output: "SOS",
+// };
+
+// oldData.push(newData);
+
+// writeTrainingData("trainOnThis.Json", oldData);
+
+// console.log(readtraingData("trainOnThis.Json"));
+
+const trainingData = readTrainingData("trainOnThis.Json");
+console.log(trainingData);
+const network = new brain.recurrent.LSTM();
+
+network.train(trainingData, {
+  iterations: 1000,
+  //   logPeriod: 1000,
+  errorThresh: 0.005,
+  log: (stats) => console.log(stats),
+});
+
+console.log("Value: ", network.run(".-"));
